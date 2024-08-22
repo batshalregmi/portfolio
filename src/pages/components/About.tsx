90e53702304631c0f661ef33c1198fcd1ca9c79c
Batshal Regmi <35691890+batshalregmi@users.noreply.github.com>
2024-08-22 19:50:05 -0400

    Update About.tsx

--- src/pages/components/About.tsx
+++ src/pages/components/About.tsx
@@ -43,7 +43,7 @@ const About = () => {
                 GitHub
               </a>
               <a
-                href="#"
+                href="https://drive.google.com/file/d/1kTiB3Lp_Vnj3ZFw0VtEDgk3UEsOqoNSo/view?usp=drive_link"
                 className="inline-flex items-center justify-center rounded-md bg-[#1a1a1a] px-6 py-3 text-sm font-medium text-gray-50 shadow-sm transition-colors hover:bg-[#2a2a2a] focus:outline-none focus:ring-2 focus:ring-[#4a4a4a] focus:ring-offset-2"
                 target="_blank"
               >
